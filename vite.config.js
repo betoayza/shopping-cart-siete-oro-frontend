09781ef96c92be1af37719965c93fdf926c16378
Alberto <aaayza@gmail.com>
2022-06-26 23:12:45 -0300

    Testing

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: {
-      type: string | boolean,
-      default: '127.0.0.1'
+    host: "127.0.0.1",    
+    port: 12345,
+    proxy: {
+      '/api': "http://localhost:4000"
     },
-    port: {
-      type: number,
-      default: 12345
-    }
-  }
-})
+  },
+  // base: "/shopping-cart-siete-oro-frontend/",
+});
