d2a7c339c052d3348049386fdf3d61b2c2a5c928
betoayza <aaayza@gmail.com>
2022-10-03 23:00:25 -0300

    Success Payment updated

--- src/components/NavBarUser.jsx
+++ src/components/NavBarUser.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export const NavBarUser = ({ code }) => {
   return (
--- src/components/SuccessPayment.jsx
+++ src/components/SuccessPayment.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
 import axios from "axios";
-import { ShoppingCartTable } from "./ShoppingCartTable";
 import { NavBarUser } from "./NavBarUser";
+import { faIgloo } from "@fortawesome/free-solid-svg-icons";
 
 export const SuccessPayment = () => {
   const [shoppingCart, setShoppingCart] = useState({ products: [] });
+  const [orderAdded, setOrderAdded] = useState(false);
+  const [itemsRemoved, setItemsRemoved] = useState(false);
+  const [products, setProducts] = useState(null);
   let navigate = useNavigate(null);
   let location = useLocation();
+
   let { userCode } = useParams();
 
   console.log(location.pathname, " | ", userCode);
 
   useEffect(() => {
+    //mount fase
     //1) get all items from shopping cart
     const getShoppingCart = async () => {
       const options = {
@@ -31,79 +36,97 @@ export const SuccessPayment = () => {
         .request(options)
         .then((res) => {
           console.log(res.data);
-          if (res.data) setShoppingCart(res.data);
+          if (res.data) {
+            //alert("Carrito vacio obtenido");
+            setShoppingCart(res.data);
+          }
         })
         .catch((error) => {
           console.error(error);
         });
     };
     getShoppingCart();
-    //2) register order
-    const addOrder = async () => {
-      let items = shoppingCart.products;
-      const options = {
-        url: "/api/user/orders/add",
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "*",
-          Accept: "application/json",
-          timeout: 3000,
-        },
-        data: { userCode, items },
-      };
 
-      await axios
-        .request(options)
-        .then((res) => {
-          console.log(res.data);
-          if (res.data) {
-            alert("Orden agregada ;)");
-          } else {
-            alert("No se pudo registrar el pedido :(");
-          }
-        })
-        .catch((error) => error);
-    };
-    addOrder();
+    //unmount fase
+    return () => {
+      if (shoppingCart.products.length) {
+        //2) register order
+        const addOrder = async () => {
+          const items = shoppingCart.products.map((product) => ({
+            ...product,
+            image: "",
+          }));
 
-    //3) clean shopping cart
-    const removeAllItems = async () => { //working
-      // working
-      const options = {
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "*",
-          Accept: "application/json",
-          timeout: 3000,
-        },
-        data: { userCode },
-      };
-      await axios
-        .delete("/api/user/shopping-cart/delete/all", options)
-        .then((res) => {
-          console.log(res.data);
-          if (res.data) {            
-            setShoppingCart(res.data);
-          }
-        })
-        .catch((error) => error);
+          const options = {
+            url: "/api/user/orders/add",
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "*",
+              Accept: "application/json",
+              timeout: 3000,
+            },
+            data: { userCode, items },
+          };
+
+          await axios
+            .request(options)
+            .then((res) => {
+              console.log(res.data);
+              if (res.data) {
+                //alert("Orden agregada ;)");
+                setOrderAdded(true);
+              } else {
+                alert("No se pudo registrar el pedido :(");
+              }
+            })
+            .catch((error) => error);
+        };
+        //if (shoppingCart.products.length > 0)
+        addOrder();
+
+        //3) clean shopping cart
+        const removeAllItems = async () => {
+          //working
+          // working
+          const options = {
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "*",
+              Accept: "application/json",
+              timeout: 3000,
+            },
+            data: { userCode },
+          };
+          await axios
+            .delete("/api/user/shopping-cart/delete/all", options)
+            .then((res) => {
+              console.log(res.data);
+              if (res.data) {
+                setItemsRemoved(true);
+              }
+            })
+            .catch((error) => error);
+        };
+        //if (shoppingCart.products.length > 0)
+        removeAllItems();
+      }
     };
-    removeAllItems();
   }, []);
 
-  return (
+  return orderAdded && itemsRemoved ? (
     <div className={"nav-bar"}>
       <NavBarUser code={userCode} />
       <h2>Mi carrito</h2>
-      <ShoppingCartTable
-        shoppingCart={shoppingCart}
-        setShoppingCart={setShoppingCart}
-        userCode={userCode}
-      />
+      <h3>Carrito vacío</h3>
       <h2>Gracias por su compra ;)</h2>
+    </div>
+  ) : (
+    <div className={"nav-bar"}>
+      <NavBarUser code={userCode} />
+      <h1>Un error ocurrió :(</h1>
     </div>
   );
 };
