dd807d4f694a01de7143f36bb458ab1f437de56e
betoayza <aaayza@gmail.com>
2022-10-20 21:49:51 -0300

    Testing vitest

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,6 +1,5 @@
 import { describe, expect, test } from "vitest";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom";
 
 describe("Home test", () => {
   //render component before each test
@@ -12,6 +11,6 @@ describe("Home test", () => {
   test("Should show something", () => {
     // const {getByText} = render(<Home />);
     // expect(getByText('Bienvenido')).toBeInTheDocument();
-    expect(Math.sqrt(4)).toBe(2);
+    expect(2).toBe(2);
   });
 });
