e87b75f302c41d6639b9debd0433b2c2c2e1c80f
betoayza <aaayza@gmail.com>
2023-04-10 21:57:59 -0300

    Change State Order updated

--- src/components/container/OrdersTable.jsx
+++ src/components/container/OrdersTable.jsx
@@ -8,7 +8,6 @@ import { SearchingBarOrders } from "./SearchingBarOrders";
 import { SearchUser } from "./SearchUser";
 import { Loader } from "../pure/Loader";
 import { helpAxios } from "../../helpers/helpAxios";
-const [isError, setIsError] = useState(false)
 
 export const OrdersTable = ({
   orders,
@@ -16,14 +15,16 @@ export const OrdersTable = ({
   showSearchingBar = true,
   isModalStyle = false,
 }) => {
+  const [products, setProducts] = useState([]);
+  const [term, setTerm] = useState("");
   const [modal, setModal] = useState(false);
   const [modalSearchOrder, setModalSearchOrder] = useState(false);
   const [modalSeeProducts, setModalSeeProducts] = useState(false);
-  const [products, setProducts] = useState([]);
-  const [modalSearchUser, setModalSearchUser] = useState(null);
+  const [modalSearchUser, setModalSearchUser] = useState(false);
+  const [modalIsStateChanged, setModalIsStateChanged] = useState(false);
   const [userCode, setUserCode] = useState(null);
-  const [term, setTerm] = useState("");
-  const [loader, setLoader] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   if (!Array.isArray(orders)) {
     orders = [orders];
@@ -45,14 +46,14 @@ export const OrdersTable = ({
     });
     console.log(itemsIDs); //works
 
+    const url = `${import.meta.env.VITE_API}/products/get/list`;
     const options = {
-      url: `${import.meta.env.VITE_API}/products/get/list`,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        Accept: "application/json",
-      },
+      // headers: {
+      //   "Content-Type": "application/json",
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Allow-Headers": "*",
+      //   Accept: "application/json",
+      // },
       timeout: 3000,
       params: { itemsIDs },
     };
@@ -63,7 +64,7 @@ export const OrdersTable = ({
         console.log(res.data);
         if (res.data) {
           setProducts(res.data);
-          setLoader(false);
+          setIsLoading(false);
         }
       })
       .catch((error) => error);
@@ -75,19 +76,26 @@ export const OrdersTable = ({
     setProducts([]);
   };
 
+  const handleCloseChangeState = () => {
+    setModal(false);
+    setModalIsStateChanged(false);
+  };
+
   const handleChangeStateOrder = async (orderCode, newState) => {
     const code = orderCode;
     const result = await helpAxios().changeOrderState(code, newState);
 
-    if(result instanceof Error) setIsError(true)
-    else {}    
-   
+    if (result instanceof Error) setIsError(true);
+    else {
+      setModal(true);
+      setModalIsStateChanged(true);
+    }
   };
 
   return modal ? (
     <Modal>
       {modalSeeProducts &&
-        (loader ? (
+        (isLoading ? (
           <Loader />
         ) : (
           <div className={""} style={{ color: "green", maxHeight: "100vh" }}>
@@ -123,6 +131,14 @@ export const OrdersTable = ({
           setModalSearchOrder={setModalSearchOrder}
         />
       )}
+      {modalIsStateChanged && (
+        <div className="text-center">
+          <h3>Estado cambiado ;)</h3>
+          <button className={"btn btn-danger"} onClick={handleCloseChangeState}>
+            Cerrar
+          </button>
+        </div>
+      )}
     </Modal>
   ) : (
     <div className={"d-grid align-content-center vw-100 h-auto"}>
@@ -138,45 +154,45 @@ export const OrdersTable = ({
         </div>
       )}
 
-      <div className={"d-flex justify-content-center vw-100 h-100"}>       
-          <div className={"w-100 h-auto"}>
-            {orders.length === 1 ? <h2>Pedido:</h2> : <h2>Pedidos:</h2>}
-            <div className={"w-100 d-flex justify-content-center"}>
-              <div
-                className={"table-responsive overflow-auto"}
-                style={{ width: "75%", maxHeight: "500px" }}
+      <div className={"d-flex justify-content-center vw-100 h-100"}>
+        <div className={"w-100 h-auto"}>
+          {orders.length === 1 ? <h2>Pedido:</h2> : <h2>Pedidos:</h2>}
+          <div className={"w-100 d-flex justify-content-center"}>
+            <div
+              className={"table-responsive overflow-auto"}
+              style={{ width: "75%", maxHeight: "500px" }}
+            >
+              <table
+                className={"table table-hover table-sm"}
+                style={isModalStyle ? { color: "#20c997" } : null}
               >
-                <table
-                  className={"table table-hover table-sm"}
-                  style={isModalStyle ? { color: "#20c997" } : null}
-                >
-                  <thead>
-                    <tr>
-                      <th scope="col">Código</th>
-                      <th scope="col">Usuario</th>
-                      <th scope="col">Productos</th>
-                      <th scope="col">Monto</th>
-                      <th scope="col">Fecha</th>
-                      <th scope="col">Estado</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {orders.map((order) => {
-                        return (
-                          <OrderTableRow
-                            key={order._id}
-                            order={order}
-                            handleSearchUser={handleSearchUser}
-                            handleSeeOrderProducts={handleSeeOrderProducts}
-                            handleChangeStateOrder={handleChangeStateOrder}
-                          />
-                        );
-                      })}
-                  </tbody>
-                </table>
-              </div>
+                <thead>
+                  <tr>
+                    <th scope="col">Código</th>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Productos</th>
+                    <th scope="col">Monto</th>
+                    <th scope="col">Fecha</th>
+                    <th scope="col">Estado</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orders.map((order) => {
+                    return (
+                      <OrderTableRow
+                        key={order._id}
+                        order={order}
+                        handleSearchUser={handleSearchUser}
+                        handleSeeOrderProducts={handleSeeOrderProducts}
+                        handleChangeStateOrder={handleChangeStateOrder}
+                      />
+                    );
+                  })}
+                </tbody>
+              </table>
             </div>
-          </div>       
+          </div>
+        </div>
       </div>
     </div>
   );
